b96f9194495b2ea2ec61bc3fec85d8cf7716f020
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-15 19:29:18 +0400

    add token type enum

---
+++ YAPL.Core/Lexer/TokenType.cs
@@ -0,0 +1,22 @@
+﻿namespace YAPL.Core.Lexer;
+
+public enum TokenType
+{
+    LET,
+    PRINT,
+
+    IDENTIFIER,
+
+    NUMBER,
+    STRING,
+
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    EQUAL,
+    DOUBLE_EQUAL,
+
+    EOF,
+    UNKNOWN
+}
