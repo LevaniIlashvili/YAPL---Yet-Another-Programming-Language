73c574c8f7e2dcb3e75c3784a202cde1c45d20e2
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-15 19:29:27 +0400

    add token class

---
+++ YAPL.Core/Lexer/Token.cs
@@ -0,0 +1,17 @@
+﻿namespace YAPL.Core.Lexer;
+
+public class Token
+{
+    public TokenType Type { get; init; }
+    public string Value { get; init; }
+    public int Line { get; init; }
+    public int Column { get; init; }
+
+    public Token(TokenType tokenType, string value, int line, int column)
+    {
+        Type = tokenType;
+        Value = value;
+        Line = line;
+        Column = column;
+    }
+}
